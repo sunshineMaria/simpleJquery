8029fc1ac2f4501910ac8be36db1c7df8f17f2b2
sunshineMaria <MLS@2015-133deMacBook-Pro.local>
2015-08-21 14:37:33 +0800

    简易jquery

--- simpleJquery.js
+++ simpleJquery.js
@@ -6,10 +6,11 @@ jQuery.fn = jQuery.prototype = {
 	init: function( selector ){
 		var selector = this.selector = document.querySelector( selector );
 		this.unwrap = function(){
-
+			var parentSelector = selector.parentNode;
+			parentSelector.parentNode.replaceChild( selector,parentSelector );
 		};
 		this.wrap = function(){
-
+			
 		};
 		this.wrapAll = function(){
 
